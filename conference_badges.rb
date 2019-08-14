ef31510f33a305722088c721f46139aa65f8e810
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-14 23:01:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,7 +16,11 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  batch_badge_creator
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
   
-  assign_rooms
+  assign_rooms(attendees).each do |room|
+    puts room
+  end
 end

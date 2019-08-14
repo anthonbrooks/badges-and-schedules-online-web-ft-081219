bc196ae1d2a13bdf84b8841583afdad851c8c0c1
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-08-14 22:48:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,10 +6,8 @@ def batch_badge_creator(names)
   i = 0
   introArr = []
   names.each do |name|
-    while (i < names.length)
+    
       print "Hello, my name is #{name}."
-      i++
-      end
+      
     end
-  end
-
+end
